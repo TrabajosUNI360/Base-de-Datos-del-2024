0cba01d3f46c06f16cc8966d47d0b114e1dfb25d
TrabajosUNI360 <146045667+TrabajosUNI360@users.noreply.github.com>
2024-04-24 12:28:32 -0600

    coneccionNOterminada

---
+++ BasedeDatos_Angel/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasedeDatos_Angel
+{
+    public class BD_Conexion
+    {
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=BDM;Data Source=MALAJIM\\SQLEXPRESS01");
+            conexion.Open();
+
+            return conexion;
+        }
+    }
+}
